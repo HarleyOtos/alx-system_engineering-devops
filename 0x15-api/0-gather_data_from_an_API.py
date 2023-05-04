a05f649bb66ab2677e486bfd0282a2a521eaf17d
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-04 19:27:06 +0100

    line validation fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script retrieves data from a REST API and
-displays information about an employee's completed tasks.
+This script retrieves data from a REST API
+and displays information about an employee's
+completed tasks.
 """
 
 import requests
@@ -10,27 +11,30 @@ import sys
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py EMPLOYEE_ID")
+        print(
+            "Usage: python3 gather_data_from_an_API.py EMPLOYEE_ID")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
 
     # Retrieve user information
     response = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+        "https://jsonplaceholder.typicode.com/users/{}"
+        .format(employee_id))
     if response.status_code != 200:
-        print("Could not retrieve user information for ID " + str(employee_id))
+        print("Could not retrieve user information for ID " 
+              + str(employee_id))
         sys.exit(1)
     user = response.json()
     employee_name = user["name"]
 
     # Retrieve TODO list information
     response = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-            employee_id))
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+            .format(employee_id))
     if response.status_code != 200:
-        print("Could not retrieve TODO list information for ID " + str(
-            employee_id))
+        print("Could not retrieve TODO list information for ID " 
+              + str(employee_id))
         sys.exit(1)
     todos = response.json()
 
