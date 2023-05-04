135baacdaf8170b09d6c4e544e42ca3b6ef49e13
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-04 21:46:43 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,55 +6,55 @@ import csv
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 gather_data_from_an_API.py EMPLOYEE_ID")
-    sys.exit(1)
 
-employee_id = int(sys.argv[1])
+def main():
+    if len(sys.argv) != 2:
+        print(
+            "Usage: python3 gather_data_from_an_API.py EMPLOYEE_ID")
+        sys.exit(1)
 
-# Retrieve user information
-response = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id))
-if response.status_code != 200:
-    print("Could not retrieve user information for ID " + str(
-        employee_id))
-    sys.exit(1)
-user = response.json()
-employee_name = user["name"]
+    employee_id = int(sys.argv[1])
 
-# Retrieve TODO list information
-response = requests.get(
-    "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        employee_id))
-if response.status_code != 200:
-    print("Could not retrieve TODO list information for ID " + str(
-        employee_id))
-    sys.exit(1)
-todos = response.json()
+    # Retrieve user information
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            employee_id))
+    if response.status_code != 200:
+        print("Could not retrieve user information for ID " + str(
+            employee_id))
+        sys.exit(1)
+    user = response.json()
+    employee_name = user["name"]
 
-# Calculate progress
-total_tasks = len(todos)
-done_tasks = len([todo for todo in todos if todo["completed"]])
+    # Retrieve TODO list information
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            employee_id))
+    if response.status_code != 200:
+        print("Could not retrieve TODO list information for ID " + str(
+            employee_id))
+        sys.exit(1)
+    todos = response.json()
 
-# Display information
-print(
-    "Employee {} is done with tasks({}/{})".format(
-        employee_name, done_tasks, total_tasks))
-for todo in todos:
-    if todo["completed"]:
-        print("\t {}".format(todo['title']))
-    else:
-        print("\t[X] {}".format(todo['title']))
+    # Export data to CSV
+    filename = "{}.csv".format(employee_id)
+    with open(filename, mode='w', newline='') as csv_file:
+        fieldnames = ['USER_ID', 'USERNAME',
+                      'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-# Export to CSV
-if todos:
-    with open("{}.csv".format(employee_id), mode="w", 
-              newline="") as csv_file:
-        writer = csv.writer(
-            csv_file, delimiter=',', quotechar='"', 
-                quoting=csv.QUOTE_MINIMAL)
+        writer.writeheader()
+
         for todo in todos:
-            writer.writerow([
-                employee_id, employee_name,
-                todo["completed"], todo["title"]])
+            writer.writerow({
+                'USER_ID': employee_id,
+                'USERNAME': employee_name,
+                'TASK_COMPLETED_STATUS': str(todo['completed']),
+                'TASK_TITLE': todo['title']
+            })
+
+    print("Data exported to {}".format(filename))
+
+
+if __name__ == "__main__":
+    main()
