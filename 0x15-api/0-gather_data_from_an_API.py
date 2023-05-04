4fae9704a898ab4c1cb45cbf7a964097145efd8b
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-04 18:29:09 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,49 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """
-This script retrieves data from a REST API
-and displays information about an employee's completed tasks.
+This script retrieves data from a REST API and
+displays information about an employee's completed tasks.
 """
 
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 gather_data_from_an_API.py EMPLOYEE_ID")
-    sys.exit(1)
 
-employee_id = int(sys.argv[1])
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py EMPLOYEE_ID")
+        sys.exit(1)
 
-# Retrieve user information
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-if response.status_code != 200:
-    print(f"Could not retrieve user information for ID {employee_id}")
-    sys.exit(1)
-user = response.json()
-employee_name = user["name"]
+    employee_id = int(sys.argv[1])
 
-# Retrieve TODO list information
-response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
-if response.status_code != 200:
-    print(f"Could not retrieve TODO list information for ID {employee_id}")
-    sys.exit(1)
-todos = response.json()
+    # Retrieve user information
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    if response.status_code != 200:
+        print(f"Could not retrieve user information for ID {employee_id}")
+        sys.exit(1)
+    user = response.json()
+    employee_name = user["name"]
 
-# Calculate progress
-total_tasks = len(todos)
-done_tasks = len([todo for todo in todos if todo["completed"]])
+    # Retrieve TODO list information
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    if response.status_code != 200:
+        print(f"Could not retrieve TODO list information for ID {employee_id}")
+        sys.exit(1)
+    todos = response.json()
 
-# Display information
-print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
-for todo in todos:
-    if todo["completed"]:
-        print(f"\t {todo['title']}")
+    # Calculate progress
+    total_tasks = len(todos)
+    done_tasks = len([todo for todo in todos if todo["completed"]])
+
+    # Display information
+    print(
+        f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+    for todo in todos:
+        if todo["completed"]:
+            print(f"\t {todo['title']}")
+
+
+if __name__ == "__main__":
+    main()
