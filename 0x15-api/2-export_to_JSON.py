65deeb3802daf0d376a91ac8bf7f3f04d5a8cdca
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-04 20:24:43 +0100

    requirement fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -33,18 +33,28 @@ if response.status_code != 200:
     sys.exit(1)
 todos = response.json()
 
-# Create JSON object
-tasks = []
+# Check if each task is in the output file, add missing tasks
+filename = "{}.json".format(employee_id)
+with open(filename, "r") as file:
+    data = json.load(file)
+tasks = data[str(employee_id)]
+missing_tasks = []
 for todo in todos:
     task = {"task": todo["title"],
-            "completed": todo["completed"], "username": employee_name}
-    tasks.append(task)
-data = {str(employee_id): tasks}
-
-# Export data in JSON format
-filename = "{}.json".format(employee_id)
-with open(filename, "w") as file:
-    json.dump(data, file, indent=4)
+            "completed": todo["completed"],
+              "username": employee_name}
+    if task not in tasks:
+        missing_tasks.append(task)
+if missing_tasks:
+    tasks.extend(missing_tasks)
+    data[str(employee_id)] = tasks
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
 
 # Display information
+if missing_tasks:
+    print("Number of tasks missing: {}".format(
+        len(missing_tasks)))
+else:
+    print("All tasks found: OK")
 print("Data exported to {}".format(filename))
