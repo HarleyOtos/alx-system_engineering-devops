d5912f4cc017bff90578a4dbee53be0de868f54d
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-04 20:35:26 +0100

    code updated due to requirement failure

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,50 +1,58 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """
-This Python script export data in the JSON format
+This script retrieves data from a REST API and exports it in JSON format
 """
 import json
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 gather_data_from_an_API.py EMPLOYEE_ID")
-    sys.exit(1)
 
-employee_id = int(sys.argv[1])
+def main():
+    if len(sys.argv) != 2:
+        print(
+            "Usage: python3 gather_data_from_an_API.py EMPLOYEE_ID")
+        sys.exit(1)
 
-# Retrieve user information
-response = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id))
-if response.status_code != 200:
-    print("Could not retrieve user information for ID " + str(
-        employee_id))
-    sys.exit(1)
-user = response.json()
-employee_name = user["name"]
+    employee_id = int(sys.argv[1])
 
-# Retrieve TODO list information
-response = requests.get(
-    "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        employee_id))
-if response.status_code != 200:
-    print("Could not retrieve TODO list information for ID " + str(
-        employee_id))
-    sys.exit(1)
-todos = response.json()
+    # Retrieve user information
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            employee_id))
+    if response.status_code != 200:
+        print("Could not retrieve user information for ID " + str(
+            employee_id))
+        sys.exit(1)
+    user = response.json()
+    employee_name = user["name"]
 
-# Create JSON object
-tasks = []
-for todo in todos:
-    task = {"task": todo["title"],
-            "completed": todo["completed"], "username": employee_name}
-    tasks.append(task)
-data = {str(employee_id): tasks}
+    # Retrieve TODO list information
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            employee_id))
+    if response.status_code != 200:
+        print("Could not retrieve TODO list information for ID " + str(
+            employee_id))
+        sys.exit(1)
+    todos = response.json()
 
-# Export data in JSON format
-filename = "{}.json".format(employee_id)
-with open(filename, "w") as file:
-    json.dump(data, file, indent=4)
+    # Create JSON object
+    tasks = []
+    for todo in todos:
+        task = {"task": todo["title"],
+                "completed": todo["completed"], "username": employee_name}
+        tasks.append(task)
+    data = {str(employee_id): tasks}
 
-# Display information
-print("Data exported to {}".format(filename))
+    # Export data in JSON format
+    filename = "{}.json".format(employee_id)
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+
+    # Display information
+    print("Data exported to {}".format(filename))
+
+
+if __name__ == "__main__":
+    main()
